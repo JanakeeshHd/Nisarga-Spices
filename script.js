7d901835cfafca98df14d77e5640a3f72213c1d6
Janakeesh HD <160233936+JanakeeshHd@users.noreply.github.com>
2026-01-14 20:13:43 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -16,7 +16,7 @@
  */
 
 // Configuration - CUSTOMIZE THESE VALUES
-const WHATSAPP_PHONE_NUMBER = '+91 8971193507';
+const WHATSAPP_PHONE_NUMBER = '+919449202183';
 const PRODUCT_DATA = {
     honey: {
         name: 'Honey',
@@ -412,4 +412,3 @@ Customization Guide:
 
 For support or questions, contact the developer.
 `);
-
